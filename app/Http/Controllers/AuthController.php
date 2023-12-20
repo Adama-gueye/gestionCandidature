51bf58e5e6eab4d19a5abc1f5c418c54e77d29b4
Adama Gueye <adamarahma99@gmail.com>
2023-12-20 10:40:49 +0000

    documentation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *      title="Authentification API",
+ *      version="1.0.0",
+ *      description="API Documentation for user authentication"
+ * )
+ */
+
+/**
+ * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      type="http",
+ *      scheme="bearer",
+ *      bearerFormat="JWT",
+ * )
+ */
 
 class AuthController extends Controller
 {
@@ -20,9 +36,28 @@ class AuthController extends Controller
     }
 
     /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     *     path="/api/auth/login",
+     *     summary="Authenticate user and get JWT token",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="email", type="string", format="email", example="user@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="password")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="access_token", type="string", description="JWT access token"),
+     *             @OA\Property(property="token_type", type="string", description="Bearer"),
+     *             @OA\Property(property="expires_in", type="integer", description="Token expiration time in seconds")
+     *         )
+     *     ),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
      */
     public function login()
     {
@@ -36,9 +71,20 @@ class AuthController extends Controller
     }
 
     /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Get(
+     *     path="/api/auth/me",
+     *     summary="Get the authenticated user",
+     *     tags={"Authentication"},
+     *     security={"bearerAuth"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="user", type="object", ref="#/components/schemas/User")
+     *         )
+     *     ),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
      */
     public function me()
     {
@@ -46,9 +92,20 @@ class AuthController extends Controller
     }
 
     /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     *     path="/api/auth/logout",
+     *     summary="Log the user out (Invalidate the token)",
+     *     tags={"Authentication"},
+     *     security={"bearerAuth"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successfully logged out",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", description="Logout message")
+     *         )
+     *     ),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
      */
     public function logout()
     {
@@ -58,9 +115,22 @@ class AuthController extends Controller
     }
 
     /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     *     path="/api/auth/refresh",
+     *     summary="Refresh a token",
+     *     tags={"Authentication"},
+     *     security={"bearerAuth"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="access_token", type="string", description="JWT access token"),
+     *             @OA\Property(property="token_type", type="string", description="Bearer"),
+     *             @OA\Property(property="expires_in", type="integer", description="Token expiration time in seconds")
+     *         )
+     *     ),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
      */
     public function refresh()
     {
@@ -68,22 +138,68 @@ class AuthController extends Controller
     }
 
     /**
+     * @OA\Post(
+     *     path="/api/auth/register",
+     *     summary="Register a new user",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="name", type="string", example="John Doe"),
+     *             @OA\Property(property="email", type="string", format="email", example="user@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="password"),
+     *             @OA\Property(property="password_confirmation", type="string", format="password", example="password")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User registered successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="user", type="object", ref="#/components/schemas/User"),
+     *             @OA\Property(property="access_token", type="string", description="JWT access token"),
+     *             @OA\Property(property="token_type", type="string", description="Bearer"),
+     *             @OA\Property(property="expires_in", type="integer", description="Token expiration time in seconds")
+     *         )
+     *     ),
+     *     @OA\Response(response=422, description="Validation Error")
+     * )
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = auth()->login($user);
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
      * Get the token array structure.
      *
      * @param  string $token
-     *
      * @return \Illuminate\Http\JsonResponse
      */
     protected function respondWithToken($token)
     {
         return response()->json([
+            'user' => auth()->user(),
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
     }
-    /**
-     * creation compte
-     */
-
 }
--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -2,41 +2,73 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *      title="Compte API",
+ *      version="1.0.0",
+ *      description="API Documentation for managing user accounts"
+ * )
+ */
 
 class CompteController extends Controller
 {
-
-public function register(Request $request)
+    /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     summary="Register a new user account",
+     *     tags={"Compte"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="nom", type="string", example="John"),
+     *             @OA\Property(property="prenom", type="string", example="Doe"),
+     *             @OA\Property(property="tel", type="string", example="123456789"),
+     *             @OA\Property(property="age", type="integer", example=25),
+     *             @OA\Property(property="email", type="string", format="email", example="user@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="password")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User account created successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", description="Account created successfully"),
+     *             @OA\Property(property="user", type="object", ref="#/components/schemas/User"),
+     *         )
+     *     ),
+     *     @OA\Response(response=401, description="Validation Error")
+     * )
+     */
+    public function register(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'nom' => 'required|string',
+            'prenom' => 'required|string',
+            'tel' => 'required|string',
+            'age' => 'required|integer',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
 
-            $validator = Validator::make($request->all(), [
-                'email' => 'required|email',
-                'password' => 'required',
-                'nom' => 'required',
-                'prenom' => 'required',
-                'tel' => 'required',
-                'age' => 'required',
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 401);
+        }
 
-            ]);
-       
-            if($validator->fails()){
-    
-                return  response()->json(['message' => $validator->errors()],401);
-            }
         $user = new User();
         $user->nom = $request->nom;
         $user->prenom = $request->prenom;
         $user->tel = $request->tel;
         $user->age = $request->age;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = bcrypt($request->password);
         $user->role = "candidat";
         $user->save();
-        return response()->json("compte creer avec succes : $user",201);
-        
+
+        return response()->json(['message' => 'Account created successfully', 'user' => $user], 201);
     }
 }
